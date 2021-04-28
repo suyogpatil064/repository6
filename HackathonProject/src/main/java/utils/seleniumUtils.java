bbfb9beb502bf0361d2b44762844aaeaf492a877
suyogpatil064 <suyog.patil064@gmail.com>
2021-04-28 15:34:09 +0530

    some selenium utils and webdriver manager added

--- HackathonProject/src/main/java/utils/seleniumUtils.java
+++ HackathonProject/src/main/java/utils/seleniumUtils.java
@@ -1,8 +1,21 @@
 package utils;
 
+import java.util.Collections;
+import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.firefox.ProfilesIni;
+import org.openqa.selenium.opera.OperaDriver;
 import org.openqa.selenium.support.ui.Select;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class seleniumUtils {
 	
@@ -73,6 +86,52 @@ public static WebDriver driver;
 	}
 	public static void forward() {
 		driver.navigate().forward();
+	}
+	
+	/* setup driver*/
+	public static WebDriver getDriver(String browser) {
+		if (browser.equalsIgnoreCase("chrome")) {
+//			System.setProperty("webdriver.chrome.driver",
+//					System.getProperty("user.dir") + "\\resources\\drivers\\chromedriver.exe");
+			WebDriverManager.chromedriver().version("90.0.4430.24").setup();
+			ChromeOptions options = new ChromeOptions();
+			options.setExperimentalOption("excludeSwitches", Collections.singletonList("enable-automation"));
+			driver = new ChromeDriver(options);
+		} else if (browser.equalsIgnoreCase("firefox")) {
+//			System.setProperty("webdriver.gecko.driver",
+//					System.getProperty("user.dir") + "\\resources\\drivers\\geckodriver.exe");
+			WebDriverManager.firefoxdriver().setup();
+			//ProfilesIni settings = new ProfilesIni();
+			//FirefoxProfile profile = settings.getProfile("selenium");
+			FirefoxOptions firefoxOptions = new FirefoxOptions();
+			firefoxOptions.setProfile(new FirefoxProfile());
+			driver = new FirefoxDriver(firefoxOptions);
+
+			// driver = new FirefoxDriver();
+		} else if (browser.equalsIgnoreCase("edge")) {
+//			System.setProperty("webdriver.edge.driver",
+//					System.getProperty("user.dir") + "\\resources\\drivers\\msedgedriver.exe");
+					WebDriverManager.edgedriver().setup();
+					ChromeOptions chromeOptions = new ChromeOptions();
+			        chromeOptions.setBinary(
+			                "C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe");
+			        EdgeOptions edgeOptions = new EdgeOptions().merge(chromeOptions);
+			        driver = new EdgeDriver(edgeOptions);
+			driver = new EdgeDriver();
+		} else if (browser.equalsIgnoreCase("opera")) {
+//			System.setProperty("webdriver.opera.driver",
+//					System.getProperty("user.dir") + "\\resources\\drivers\\operadriver.exe");
+			WebDriverManager.operadriver().setup();
+			driver = new OperaDriver();
+		}
+
+		driver.manage().window().maximize();
+		driver.manage().timeouts().pageLoadTimeout(180, TimeUnit.SECONDS); // page load time out
+		driver.manage().timeouts().implicitlyWait(80, TimeUnit.SECONDS); //No such element found
+
+		String baseUrl = "https://www.flipkart.com";
+		driver.get(baseUrl);
+		return driver;
 	}
 	
 }
